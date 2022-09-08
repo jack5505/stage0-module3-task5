b3114196d7edc0d2e7125e33dce9e0958f6d55bf
Jack5505 <jahon.080879@gmail.com>
2022-09-08 10:30:06 +0500

    LearnJack

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        var first = 1;
+        var second = 10;
+        var third = 100;
+        System.out.println(first + "" + second + "" + third);
     }
 }
